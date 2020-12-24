7dbdb0e665427881d4f59fa949cb1999b30dcad3
BatuhanKurubacak <68086926+BatuhanKurubacak@users.noreply.github.com>
2020-12-25 01:38:28 +0300

    Updating Visulazation and Regressions

--- New_Datasets_backup/visualization.py
+++ New_Datasets_backup/visualization.py
@@ -3,6 +3,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+#from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
 
 
 data=pd.read_csv("age_data_value.csv")
@@ -32,6 +36,10 @@ print(data_c['Country'].unique())
 for x in range(70):
     
     data_c.Country=data_c['Country'].str.replace(country[x],str(x))
+
+
+data_c.Country=data_c['Country'].str.replace('49ia','50')
+data_c.Country=data_c['Country'].str.replace('15 Democratic Republic','16')
     
 
 
@@ -76,13 +84,75 @@ print('length of Age3 and Men: '+str(len(age3_male)))
 
 average_list=[]
 
-for x in range(70):       #we should take a average value of questions
+      #we should take a average value of questions
 
-    country1=age3_male[age3_male['Country']].str.contains(str(x))
-    values=country1.Value.values()
-    s=0
-    for x in values:
-        s=s+x
-    average=s/6
-    average_list.append(average)
+data_c.Country=data_c['Country'].str.replace(country[x],str(x))
 
+
+for x in range(0,420,6):
+
+    val1=age3_male.iloc[x:x+6,0:2]
+    #print(val1)
+    val1['Value'].apply(float)
+    s1=val1['Value'].sum()
+    s1=s1/6
+    s1=format(round(s1,2))
+    average_list.append(s1)
+    
+
+city_list=[]
+
+for x in range(0,70):
+    city_list.append(x)
+
+df = pd.DataFrame(city_list, columns=["Country"]) 
+df['Average_Value'] = average_list
+df=df.sort_values(by=['Average_Value'])
+
+
+
+country=df.Country.values.reshape(-1,1)
+val=df.Average_Value.values.reshape(-1,1)
+
+print(val)
+
+regression=LinearRegression()
+regression=regression.fit(country,val)
+
+
+df=df.astype(float)
+
+x=np.arange(min(df.Country),max(df.Country)).reshape(-1,1)
+plt.plot(x,regression.predict(x),color='red')
+plt.scatter(df.Country,df.Average_Value)
+plt.xlabel("Countries")
+plt.ylabel("Rate of Approval")
+plt.title("Linear Regression")
+plt.show()
+
+#print(r2_score(val,regression.predict(country)))
+'''
+regression_tree=DecisionTreeRegressor() # i used decision tree regressor but it's not make sense
+regression_tree.fit(country,val)
+print(r2_score(val,regression_tree.predict(country)))
+
+
+plt.scatter(df.Country,df.Average_Value,color='red')
+x=np.arange(min(country),max(country),0.01).reshape(-1,1)
+plt.plot(x,regression_tree.predict(x),color='blue')
+plt.xlabel('Level')
+plt.ylabel('Salary')
+plt.title('Decision Tree Model')
+plt.show()
+'''
+'''
+for x in range(10,70):                                               #alternative calculate average rate of approval but there is a mistake 
+    country1=age3_male[age3_male['Country'].str.contains(str(x))]
+    values=country1.iloc[:,1]
+    #print(type(values))
+    values.apply(float)
+    s=values.sum()
+    s=s/6
+    s=format(round(s, 2))
+    average_list.append(s)
+''' 
